a10921ebb7e0194477203060806721fb920c48da
Rajesh <rajeshvandadi1998@gmail.com>
2025-06-11 23:54:04 +0530

    V00.000.002

---
+++ Src/V00.000.002/Jio_Remote_595/PIN_CONFIG.h
@@ -0,0 +1,22 @@
+
+
+#define DATA_PIN D1  // 5
+#define CLK_PIN D3   // 0
+#define LATCH_PIN D4 // 2
+
+#define IR_LED_PIN D2 //4 
+
+#define IR_RECEIVE_PIN D5 //14
+
+
+
+
+
+/*
+int DATA_PIN = 5; 
+int CLK_PIN = 0;
+int LATCH_PIN = 2;
+int IR_LED_PIN = 4;
+int IR_RECEIVE_PIN = 14;
+
+*/
